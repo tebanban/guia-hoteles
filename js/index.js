ba686a19f4f69e2034402b66bee2b4d13e97dca2
tebanban <estebancalf@gmail.com>
2021-03-12 13:06:49 +0000

    initial commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+$(function (){
+	$('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+    $('.carousel').carousel({interval: 3000
+    });
+
+
+    $('#info').on('show.bs.modal', function (e) {
+       console.log('el modal se muestra'); 
+     
+
+        $('#infoBtn').removeClass ('btn-info');
+        $('#infoBtn').addClass ('btn-dark');
+        $('#infoBtn').prop ('disabled', true);
+        });
+
+
+
+    $('#info').on('shown.bs.modal', function (e) {
+    console.log('el modal fue mostrado'); 
+    });
+
+    $('#info').on('hide.bs.modal', function (e) {
+    console.log('el modal se oculta'); 
+    });
+
+    $('#info').on('hidden.bs.modal', function (e) {
+    console.log('el modal fue ocultado'); 
+        $('#infoBtn').removeClass ('btn-dark');
+        $('#infoBtn').addClass ('btn-info');
+        $('#infoBtn').prop ('disabled', false);
+        });
+    });        
+
+    
+	$("#footer").load("footer.html"); // carga el footer
+		
+	  
+	
